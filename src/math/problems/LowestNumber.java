ea3b25351fef4bcadab000bf77131dbc660f971e
Prithul <prithul28@gmail.com>
2018-12-24 19:29:51 -0500

    Lowest Number Commit

--- src/math/problems/LowestNumber.java
+++ src/math/problems/LowestNumber.java
@@ -1,34 +1,70 @@
 package math.problems;
 
-import databases.ConnectToSqlDB;
+/*
+ * Write java solution to find the lowest number from this array.
+ * Use one of the databases from mysql or mongodb to store and to retrieve.
+ */
 
+import databases.ConnectToSqlDB;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LowestNumber {
 
 	public static void main(String[] args) {
-		/*
-		 * Write java solution to find the lowest number from this array.
-		 * Use one of the databases from mysql or mongodb to store and to retrieve.
-		 */
+
 		int  array[] = new int[]{211,110,99,34,67,89,67,456,321,456,78,90,45,32,56,78,90,54,32,123,67,5,679,54,32,65};
 
 		//find lowest number from the array
+		System.out.print("Given Array: ");
+		for(int in : array){
+			System.out.print(" " + in);
+		}
+
+		System.out.println("\nLowest Number: : " + getLowest(array,26));
+
+		// convert array to arrayList
+		List<Integer> list = new ArrayList<Integer>();
+
+		for(int i : array){
+			list.add(i);
+		}
+		System.out.println("My arraylist: " + list);
+
 
 		ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
-		List<String> lowestValue = new ArrayList<String>();
-		try {
-			connectToSqlDB.insertDataFromArrayToSqlTable(array, "tbl_lowestNumber", "column_lowestNumber");
-			lowestValue = connectToSqlDB.readDataBase("tbl_lowestNumber", "column_lowestNumber");
 
+		List<String> lowestValue = new ArrayList<String>();
+
+		try {
+			connectToSqlDB.insertDataFromArrayListToSqlTable_1(list, "lowestnumber", "number");
+			lowestValue = connectToSqlDB.readDataBase("lowestnumber", "number");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println("Data is reading from the Table (tbl_primenumber) and displaying to the console");
-		for(String st:lowestValue){
-			System.out.println(st);
+
+		System.out.println("Data is reading from the Table (lowestnumber) and displaying to the console");
+
+		for (String st : lowestValue) {
+			System.out.print(st + " ");
 		}
+	}
+
+	public static int getLowest(int[] a, int total){
+		int temp;
+		for (int i = 0; i < total; i++)
+		{
+			for (int j = i + 1; j < total; j++)
+			{
+				if (a[i] > a[j])
+				{
+					temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+				}
+			}
+		}
+		return a[0];
 	}
 
 }
